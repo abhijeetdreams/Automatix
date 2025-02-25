ccb700cdb285b46b30c174abf22f59127806de91
abhijeetdreams <abhijit.anarase@dreamsinternational.in>
2025-02-25 12:07:22 +0530

    fix: correct conditional check in sendMessageback utility for userId and message validation

--- automatix-backend/utils/echoBotUtils.js
+++ automatix-backend/utils/echoBotUtils.js
@@ -4,7 +4,7 @@ const slackClient = new WebClient(process.env.BOT_TOKEN);
 
 const sendMessageback = async (userId, message, files = []) => {
     try {
-        if (!userId || !message) {
+        if (!userId || !message ) {
             return;
         }
 
